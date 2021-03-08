c92f8bd8df5951d5b9a6615898fcc705e33c9206
all persons <35461002238898@mail.ru>
2021-03-09 02:41:09 +0300

    add new cycle

--- attachment/js/array.js
+++ attachment/js/array.js
@@ -73,12 +73,20 @@ const data = [
     ];
 
 let sum = 0;
+
+let res = 0;
   
 for (let key in data) {
     sum += data[key].quantity * data[key].price
 }
 
+for (let type in data) {
+    res += data[type].type.PRODUCT_TYPE * data[type].price
+}
+
 console.log(sum)
+
+console.log(res)
 
 
 
