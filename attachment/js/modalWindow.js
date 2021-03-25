e676ae20863631a614743e146d425f2bc3ee1a0d
all persons <35461002238898@mail.ru>
2021-03-25 03:37:34 +0300

    correct code

--- attachment/js/modalWindow.js
+++ attachment/js/modalWindow.js
@@ -2,19 +2,19 @@ let btnShowWindow = document.querySelector('.show_modal');
 let btnCloseWindow = document.querySelector('.close_btn');
 let modalWindow = document.querySelector('.modal');
 
-function hideModalWindow() {
-    modalWindow.classList.add('hide')
-};
+// function hideModalWindow() {
+//     modalWindow.classList.add('hide')
+// };
 
-btnShowWindow.addEventListener('click', function() {
-    modalWindow.classList.remove('hide')  
-});
+// btnShowWindow.addEventListener('click', function() {
+//     modalWindow.classList.remove('hide')  
+// });
 
-btnCloseWindow.addEventListener('click', showAndHideModalWindow);
+btnShowWindow.addEventListener('click', showAndHideModalWindow);
 
 
-// function showAndHideModalWindow() {
-//     modalWindow.classList.toggle('hide')
-// }
+function showAndHideModalWindow() {
+    modalWindow.classList.toggle('hide')
+}
 
 // btnCloseWindow.addEventListener('click', hideModalWindow); // 
